4c84e4d5eb736e37ae819dcfad99a51ef2cb8bd9
Valentin Baryshev <bajiuk@yandex-team.ru>
2018-03-13 09:54:33 +0300

    Using api 24 & instance counter

--- app/src/main/java/com/bajiuk/medium/single/MainActivity.kt
+++ app/src/main/java/com/bajiuk/medium/single/MainActivity.kt
@@ -2,11 +2,24 @@ package com.bajiuk.medium.single
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        ++INSTANCES
+        Log.d(TAG,"There are $INSTANCES of a Activity in memory now.")
+    }
+
+    override fun onDestroy() {
+        --INSTANCES
+        super.onDestroy()
+    }
+
+    companion object {
+        var INSTANCES = 0
+        val TAG = MainActivity::class.java.simpleName
     }
 }
